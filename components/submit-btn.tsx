cac794e405b9b38543f52d528c1b72968ab0efbb
Beaowulf <68222371+Beaowulf@users.noreply.github.com>
2023-10-19 22:22:53 +0400

    Update submit-btn.tsx

--- components/submit-btn.tsx
+++ components/submit-btn.tsx
@@ -1,23 +1,34 @@
-import React from "react";
-import { useFormStatus as useFormStatus } from "react-dom";
-
+import React, { useState } from "react";
 import { FaPaperPlane } from "react-icons/fa";
 
 export default function SubmitBtn() {
-  const { pending } = useFormStatus();
+  const [pending, setPending] = useState(false);
+
+  const handleSubmit = () => {
+    setPending(true);
+
+    // Simulate a form submission (replace with your actual form submission logic)
+    setTimeout(() => {
+      // After submission is complete, set pending to false
+      setPending(false);
+    }, 2000); // Replace with the actual submission time
+
+    // You can call your API here or perform other submission actions
+  };
 
   return (
     <button
       type="submit"
-      className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950 disabled:scale-100 disabled:bg-opacity-65 dark:bg-white dark:bg-opacity-10"
+      className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 dark:bg-white dark:bg-opacity-10 disabled:scale-100 disabled:bg-opacity-65"
       disabled={pending}
+      onClick={handleSubmit}
     >
       {pending ? (
         <div className="h-5 w-5 animate-spin rounded-full border-b-2 border-white"></div>
       ) : (
         <>
           Submit{" "}
-          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 " />
+          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
         </>
       )}
     </button>
